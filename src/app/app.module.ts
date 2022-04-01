a37b7e53bdc4f69d00c75748b543b8cfc8f7b80c
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-03-31 23:50:00 -0300

    Creo componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
+import { CountdownComponent } from './components/countdown/countdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressBarComponent,
+    CountdownComponent
   ],
   imports: [
     BrowserModule,
