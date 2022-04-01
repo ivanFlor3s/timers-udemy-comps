1cea5f0fc87fab3b69039ab5ddfb8769269bd066
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-04-01 00:22:30 -0300

    countdown component contando para atras

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countdown.component.scss']
 })
 export class CountdownComponent implements OnInit {
+  @Input() counterStart: number = null
+
+  public counter: number = 0
 
   constructor() { }
 
   ngOnInit(): void {
+    this.startCountDown()
+  }
+
+  startCountDown(){
+    if(!!this.counterStart && this.counterStart > 0  ){
+      this.counter = this.counterStart
+      this.doCountDown()
+    }
+      
+   
+  }
+
+  doCountDown(){
+    setTimeout(() => {
+      this.counter = this.counter - 1
+      this.processCountDown()
+    }, 1000);
+  }
+
+  processCountDown(){
+    if(this.counter == 0){
+      console.log('termine')
+
+    } else {
+      this.doCountDown()
+    }
   }
 
 }
