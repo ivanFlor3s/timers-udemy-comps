a8387f4225e23ec9ee4a38706eec2f3600ef567f
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-04-02 17:52:50 -0300

    Agrego path para timers

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CounterDownContainerComponent } from '../components/containers/counter-down-container/counter-down-container.component';
+import { TimersContainerComponent } from '../components/containers/timers-container/timers-container.component';
 
 const routes: Routes = [
   {path: 'counter-down', component: CounterDownContainerComponent},
+  {path: 'timers', component: TimersContainerComponent},
   {path:'**', pathMatch:'full', redirectTo: 'counter-down'}
 ];
 
