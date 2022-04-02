16fad19230ffc6e6438582de9c41105cea2b7684
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-04-02 17:52:28 -0300

    segundos to display

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.scss']
 })
-export class DisplayComponent implements OnInit {
+export class DisplayComponent implements OnInit, OnChanges {
+
+  @Input() time: number = null;
+
+  minutos: string = '00';
+  segundos: string = '00';
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes.time){
+      const mins = Math.trunc(changes.time.currentValue / 60)
+      const segs = changes.time.currentValue - mins * 60
+      this.minutos = ('0' + mins).substring(-2)
+      this.segundos = ('0' + segs).substring(-2)
+    }
+  }
+
   ngOnInit(): void {
   }
+
+
 
 }
