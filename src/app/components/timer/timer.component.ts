a3dad74a12e50a6285851d8720ab7065a0371d0e
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-04-03 21:53:38 -0300

    Paso logica del container a servicio timer

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,70 +1,27 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, OnDestroy } from '@angular/core';
 import { CountdownComponent } from '../countdown/countdown.component';
 import { BaseCounterDownComponent } from '../../base/counter-down.component';
+import { TimerService } from '../../services/timer.service';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss'],
+  providers:[TimerService]
 })
-export class TimerComponent implements OnInit {
-  public paused: boolean = true;
-
+export class TimerComponent implements OnInit, OnDestroy {
+  
   @Output() onComplete = new EventEmitter<void>();
   @Input() init: number = 20;
+  
+  constructor(public timerService: TimerService){
 
-  private countdownTimerRef: any;
-  public countdown: number = 0;
-
-  get progressCountDown() {
-    return ((this.init - this.countdown) / this.init) * 100;
   }
-
-  constructor() {}
-
   ngOnInit(): void {
-    if (this.init && this.init > 0) {
-      this.countdown = this.init;
-    }
+    this.timerService.restartCountDown(this.init)
   }
 
-  restartCountDown(){
-    if (this.init && this.init > 0) {
-      this.paused = true;
-      this.clearTimeout();
-      this.countdown = this.init;
-    }
-  }
-
-  private doCountdown() {
-    this.countdownTimerRef = setTimeout(() => {
-      this.countdown = this.countdown - 1;
-      this.processCountdown();
-    }, 1000);
-  }
-
-  private processCountdown() {
-    if (this.countdown == 0) {
-      this.onComplete.emit();
-      console.log('--countdown end--');
-    } else {
-      this.doCountdown();
-    }
-  }
-
-  private clearTimeout() {
-    if (this.countdownTimerRef) {
-      clearTimeout(this.countdownTimerRef);
-      this.countdownTimerRef = null;
-    }
-  }
-
-  toggleActionButton() {
-    this.paused = !this.paused;
-    if(!this.paused){
-      this.doCountdown()
-    } else {
-      this.clearTimeout()
-    }
+  ngOnDestroy(): void {
+    this.timerService.destroy()
   }
 }
---
+++ src/app/services/timer.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TimerService {
+
+  public paused: boolean = true;
+  private countdownTimerRef: any;
+  public countdown: number = 0;
+  private init: number = 0
+
+  get progressCountDown() {
+    return ((this.init - this.countdown) / this.init) * 100;
+  }
+
+  constructor() {}
+
+  
+
+  restartCountDown(init?: number){
+    if(init){
+      this.init = init
+    }
+    if (this.init && this.init > 0) {
+      this.paused = true;
+      this.clearTimeout();
+      this.countdown = this.init;
+    }
+  }
+
+  private doCountdown() {
+    this.countdownTimerRef = setTimeout(() => {
+      this.countdown = this.countdown - 1;
+      this.processCountdown();
+    }, 1000);
+  }
+
+  private processCountdown() {
+    if (this.countdown == 0) {
+      // this.onComplete.emit();
+      console.log('--countdown end--');
+    } else {
+      this.doCountdown();
+    }
+  }
+
+  private clearTimeout() {
+    if (this.countdownTimerRef) {
+      clearTimeout(this.countdownTimerRef);
+      this.countdownTimerRef = null;
+    }
+  }
+
+  toggleActionButton() {
+    this.paused = !this.paused;
+    if(!this.paused){
+      this.doCountdown()
+    } else {
+      this.clearTimeout()
+    }
+  }
+
+  destroy(){
+    this.clearTimeout()
+  }
+}
