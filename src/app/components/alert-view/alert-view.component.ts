aaf314737abb8bf77e5a3bd7a425c0777a93187c
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-04-06 00:03:47 -0300

    Avanzado init - alert view component

--- src/app/components/alert-view/alert-view.component.ts
+++ src/app/components/alert-view/alert-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-alert-view',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertViewComponent implements OnInit {
 
+  
+  @Output() onCancel = new EventEmitter<void>();
+  @Output() onSubmit = new EventEmitter<number>();
+
+  public time:number = 0;
   constructor() { }
 
   ngOnInit(): void {
