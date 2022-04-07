4f5ba0d597228bae1df3b27f7db3e1f6b4de0912
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-04-06 23:08:00 -0300

    tabs & tab con hijos autoinvocandose en padre

--- src/app/components/tab/tab.component.ts
+++ src/app/components/tab/tab.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Tab } from 'src/app/interfaces/tab.interface';
+import { TabsComponent } from '../tabs/tabs.component';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
   styleUrls: ['./tab.component.scss']
 })
-export class TabComponent implements OnInit {
+export class TabComponent implements OnInit, Tab {
 
-  constructor() { }
+  @Input() title: string;
+  public isActive: boolean = false;
+
+  constructor(public tabComp: TabsComponent) {
+    this.tabComp.addTab(this)
+   }
 
   ngOnInit(): void {
   }
--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Tab } from 'src/app/interfaces/tab.interface';
 
 @Component({
   selector: 'app-tabs',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsComponent implements OnInit {
 
+  tabs: Tab[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  addTab(tab: Tab){
+    if(this.tabs.length == 0){
+      tab.isActive = true
+    }
+    this.tabs.push(tab);
+  }
+
+  selectTab(tab: Tab): void {
+    this.tabs.forEach( t => {
+      t.isActive = t==tab
+      return t;
+    })
+  }
+
+
 
 }
